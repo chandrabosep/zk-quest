07ec218d61a699a0694e434a90c3a0f06f88adf0
chandrabosep <chandrabosep3112@gmail.com>
2025-09-25 15:36:49 +0530

    Add initial project structure and dependencies for ZKQuest bounty platform MVP, including Prisma schema, context provider, and proof generation logic.

---
+++ src/config/index.tsx
@@ -0,0 +1,23 @@
+import { cookieStorage, createStorage, http } from "@wagmi/core";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import { mainnet, arbitrum } from "@reown/appkit/networks";
+
+export const projectId = "01183905ca682aef294e44b41d7634b3";
+
+if (!projectId) {
+	throw new Error("Project ID is not defined");
+}
+
+export const networks = [mainnet, arbitrum];
+
+//Set up the Wagmi Adapter (Config)
+export const wagmiAdapter = new WagmiAdapter({
+	storage: createStorage({
+		storage: cookieStorage,
+	}),
+	ssr: true,
+	projectId,
+	networks,
+});
+
+export const config = wagmiAdapter.wagmiConfig;
